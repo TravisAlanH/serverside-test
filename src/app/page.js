6dd93045eabf5fd074929c9f07521695e3de8436
Travis Heidelberger <Travis.H.Dev@gmail.com>
2023-07-31 13:09:46 -0500

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='w-full h-[30rem] flex flex-row justify-center item-center'>
+      {/* asdf */}
+    </div>
+  )
+}
